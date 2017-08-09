56ac93b9a4cb064f79e20d62ce67ee591e19b770
sportsquid <legoguy191@hotmail.com>
2017-08-09 09:44:22 -0500

    flatfile.c partially working

--- flatfile.c
+++ flatfile.c
@@ -1,8 +1,77 @@
 #include <stdio.h>
+#include <string.h>
+
+
+
+int writeff(char * dbname, char * variable, char * data){
+    char readval[50];
+    char strcat_var[50];
+    int i, getl;
+    int filelength = 0;
+    FILE * file;
+    
+    
+    memset(strcat_var, '\0', sizeof(strcat_var));
+    strcpy(strcat_var, dbname); 
+    strcat(strcat_var, ".txt");
+    //open file, see if it exists 
+    file = fopen(strcat_var, "r+");
+    if (!file){
+        return(-1);
+    }
+    FILE * copy = fopen("copy.txt", "w");
+
+    //see how long the file is
+    while(fgets(readval, sizeof(readval), file) != NULL){
+        filelength++;
+    }
+    printf("The file is %d lines long!\n", filelength);
+   
+    //set cursor to 0
+    fseek(file, 0, SEEK_SET);
+
+    //loop through the lines
+    for(i = 0; i < filelength + 1; i++){
+        
+        fgets(readval, sizeof(readval), file);
+        
+        //compare each line to look for wanted variable
+        memset(strcat_var, '\0', sizeof(strcat_var));
+         
+        strcpy(strcat_var, "~"); 
+        strcat(strcat_var, variable);
+        //see if the variable already exists
+        if (strncmp(readval, strcat_var , sizeof(variable) + 1) == 0){
+            fseek(file, sizeof(variable) + 1, SEEK_CUR);
+            fprintf(file, " \n~%s %s\n", variable, data);
+            puts("variable found and changed");
+            fclose(file);
+            fclose(copy);
+            remove("copy.txt");
+            return(1);
+        } 
+        //see if no match was found
+        else if (i == filelength){
+            fprintf(file, " \n~%s %s\n", variable, data);
+            puts("Variable created");
+            fclose(file);
+            fclose(copy);
+            remove("copy.txt");
+            return(1);
+        }
+    }
+    puts("failed for statment");
+    fclose(copy);
+    remove("copy.txt");
+    return(-1);
+}//end of writeff
+
+
 
 
 
 int main(){
-    FILE *pFile;
+    int test = writeff("test", "meme_type", "dank");
+    printf("%d\n", test);
 
 }
